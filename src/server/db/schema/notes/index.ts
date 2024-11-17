2d077103c3cb8bfeb5e2c0208ea40871bcd7ad91
Lucas <lucastheldl@gmail.com>
2024-11-17 01:01:00 -0300

    feature/ added relations to schema tables

--- src/server/db/schema/notes/index.ts
+++ src/server/db/schema/notes/index.ts
@@ -1,8 +1,20 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { integer, pgEnum, pgTable, varchar } from "drizzle-orm/pg-core";
+import { usersSchema } from "../users";
+
+export const colorsEnum = pgEnum("colors", ["white", "red", "blue", "green"]);
 
 export const notesSchema = pgTable("notes", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   title: varchar({ length: 255 }).notNull(),
   body: varchar().notNull(),
-  color: varchar({ length: 50 }).notNull().default("White"),
+  color: colorsEnum().default("white"),
+  authorId: integer().notNull(),
 });
+
+export const notesRelations = relations(notesSchema, ({ one }) => ({
+  author: one(usersSchema, {
+    fields: [notesSchema.authorId],
+    references: [usersSchema.id],
+  }),
+}));
--- src/server/db/schema/users/index.ts
+++ src/server/db/schema/users/index.ts
@@ -1,8 +1,14 @@
+import { relations } from "drizzle-orm";
 import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { notesSchema } from "../notes";
 
 export const usersSchema = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
-  password_hash: varchar({ length: 255 }).notNull(),
+  passwordHash: varchar({ length: 255 }).notNull(),
 });
+
+export const usersRelations = relations(usersSchema, ({ many }) => ({
+  notes: many(notesSchema),
+}));
